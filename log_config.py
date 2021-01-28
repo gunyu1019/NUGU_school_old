1964574394ba47bb6543e3c330a1e7cb6fc72a21
gunyu1019 <gunyu1019@gmail.com>
2021-01-28 22:22:01 +0900

    Logging add

---
+++ log_config.py
@@ -0,0 +1,51 @@
+import sys
+
+LOGGING_CONFIG = dict(
+    version=1,
+    disable_existing_loggers=False,
+    loggers={
+        "sanic.root": {"level": "INFO", "handlers": ["console"]},
+        "sanic.error": {
+            "level": "INFO",
+            "handlers": ["error_console"],
+            "propagate": True,
+            "qualname": "sanic.error",
+        },
+        "sanic.access": {
+            "level": "INFO",
+            "handlers": ["access_console"],
+            "propagate": True,
+            "qualname": "sanic.access",
+        },
+    },
+    handlers={
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "generic",
+            "stream": sys.stdout,
+        },
+        "error_console": {
+            "class": "logging.StreamHandler",
+            "formatter": "generic",
+            "stream": sys.stderr,
+        },
+        "access_console": {
+            "class": "logging.StreamHandler",
+            "formatter": "access",
+            "stream": sys.stdout,
+        },
+    },
+    formatters={
+        "generic": {
+            "format": "%(asctime)s - (main)[%(levelname)s]: %(message)s",
+            "datefmt": "[%Y-%m-%d %H:%M:%S]",
+            "class": "logging.Formatter",
+        },
+        "access": {
+            "format": "%(asctime)s - (%(name)s)[%(levelname)s][%(host)s]: "
+            + "%(request)s %(message)s %(status)d",
+            "datefmt": "[%Y-%m-%d %H:%M:%S]",
+            "class": "logging.Formatter",
+        },
+    },
+)
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from sanic import Sanic
 
 from config import parser
+from log_config import LOGGING_CONFIG
 from route.meal import meal
 from route.timetable import timetable
 from route.health import health
 
 if __name__ == "__main__":
-    app = Sanic(__name__)
+    app = Sanic(__name__, log_config=LOGGING_CONFIG)
 
     app.add_route(timetable, "/api/NUGU/timetable", methods=["POST"])
     app.add_route(meal, "/api/NUGU/meal", methods=["POST"])
